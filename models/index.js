9c2065c992e8f75e11c4ab9e04c6c4493e61e5bd
Madisen Vogel <madisen@Admins-Mac-mini.btm.private>
2022-11-21 14:20:15 -0700

    updated models/index.jswith geography data

--- models/index.js
+++ models/index.js
@@ -1,3 +1,29 @@
 const User = require('./User');
+const GeoPost = require('./GeoPost');
+const GeoComment = require('./GeoComment');
 
-module.exports = { User };
+User.hasMany(GeoPost, {
+    foreignKey: 'user_id'
+})
+
+User.hasMany(GeoComment, {
+    foreignKey: 'user_id'
+})
+
+GeoPost.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+GeoPost.hasMany(GeoComment, {
+    foreignKey: 'geoPost_id'
+})
+
+GeoComment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+GeoComment.belongsTo(GeoPost, {
+    foreignKey: 'geoPost_id'
+})
+
+module.exports = { User, GeoComment, GeoPost };
