9eb60d9f42c08d482d19f73d1f34b0264ce4f42d
Madisen Vogel <madisen@Admins-Mac-mini.btm.private>
2022-11-21 14:05:30 -0700

    geography post model

---
+++ models/GeoPost.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class GeoPost extends Model {}
+
+GeoPost.init({
+    geo_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    geo_title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [1]
+        }
+    },
+    geo_content: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [1]
+        }
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'user',
+            key: 'id'
+        }
+    }
+}, {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post'
+})
+
+module.exports = GeoPost;
