573d4544bee5acd61838a774815f4948266a360b
MarMarMo <108547874+MMockus15@users.noreply.github.com>
2022-11-21 19:18:17 -0700

    added all api index routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,35 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 
-router.use('/users', userRoutes);
+//Geography Routes
+const GeoPostRoutes = require('./GeoPostRoutes');
+const GeoCommentRoutes = require('./GeoCommentRoutes');
+
+//Tech Routes
+const TechPostRoutes = require('./TechPostRoutes');
+const TechCommentRoutes = require('./TechCommentRoutes');
+
+//Art Routes
+const ArtPostRoutes = require('./ArtPostRoutes');
+const ArtCommentRoutes = require('./ArtCommentRoutes');
+
+//User model for routes
+router.use('/user', userRoutes);
+
+//Use Geo models for routes
+router.use('/GeoPost', GeoPostRoutes);
+router.use('/GeoComment', GeoCommentRoutes);
+
+//Tech model for routes
+router.use('/TechPost', TechPostRoutes);
+router.use('/TechComment', TechCommentRoutes);
+
+//Art model for routes
+router.use('/ArtPost', ArtPostRoutes);
+router.use('/ArtComment', ArtCommentRoutes);
+
+
+
 
 module.exports = router;
 
