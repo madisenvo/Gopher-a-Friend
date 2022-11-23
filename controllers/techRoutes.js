d11b812036375563d6df349638fc2a8086f39fa6
Madisen Vogel <madisen@Admins-Mac-mini.btm.private>
2022-11-23 14:48:06 -0700

    added update and add post to tech routes

--- controllers/techRoutes.js
+++ controllers/techRoutes.js
@@ -35,4 +35,50 @@ router.get('/', (req, res) => {
         });
 });
 
+router.get('/update/:id', withAuth, (req, res) => {
+    TechPost.findOne({
+        attributes: [
+            'id',
+            'tech_title',
+            'tech_text',
+        ],
+        include: [{
+                model: TechComment,
+                attributes: ['id', 'tech_comment_text', 'tech_post_id', 'user_id'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+        ]
+        })
+        .then(postData => {
+            if (!postData) {
+                res.status(404).json({
+                    message: 'Uh Oh! No post found.'
+                });
+                return;
+            }
+
+            const techPost = postData.get({
+                plain: true
+            });
+
+            res.render('edit', {
+                techPost,
+                loggedIn: true
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+})
+
+router.get('/new', (req, res) => {
+    res.render('technology', {
+        loggedIn: true
+    })
+})
+
 module.exports = router;
