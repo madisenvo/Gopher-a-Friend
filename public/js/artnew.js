7c3f6fd85633d39965d0c8476f65e978cc39b35c
Madisen <madisenvogel@gmail.com>
2022-11-25 19:31:18 -0700

    art chatroom working

--- public/js/artnew.js
+++ public/js/artnew.js
@@ -2,13 +2,13 @@ const newFormHandler = async (event) => {
 	event.preventDefault();
 
 	const art_title = document.querySelector('input[name="art-post-title"]').value;
-	const art_body = document.querySelector('textarea[name="art-post-body"]').value;
+	const art_text = document.querySelector('textarea[name="art-post-body"]').value;
 
 	await fetch("/api/artpost", {
 		method: 'POST',
 		body: JSON.stringify({
 		art_title,
-		art_body
+		art_text
 		}),
 		headers: {
 		'Content-Type': 'application/json'
