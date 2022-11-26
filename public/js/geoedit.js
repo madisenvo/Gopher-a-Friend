09b6809c50d82e9f998c45b34dbb6385c5209127
Madisen <madisenvogel@gmail.com>
2022-11-25 19:42:18 -0700

    geo chatroom working

--- public/js/geoedit.js
+++ public/js/geoedit.js
@@ -1,18 +1,17 @@
-const geo_id = document.querySelector('input[name="geo-post-id"]').value;
+const geo_id = window.location.toString().split('/')[window.location.toString().split('/').length - 1];
 
 const editGeoFormHandler = async (event) => {
 	event.preventDefault();
 
 	const geo_title = document.querySelector('input[name="geo-post-title"]').value;
-	const geo_body = document.querySelector('textarea[name="geo-post-body"]').value;
+	const geo_text = document.querySelector('textarea[name="geo-post-body"]').value;
 
-	if (geo_body && geo_title) {
+	if (geo_text && geo_title) {
 		const response = await fetch(`/api/geopost/${geo_id}`, {
 		method: 'PUT',
-		geo_body: JSON.stringify({
+		body: JSON.stringify({
 		geo_title,
-		body
-		//not sure if body above should be art_body
+		geo_text
 	}),
 		headers: {
 		'Content-Type': 'application/json'
@@ -21,15 +20,15 @@ const editGeoFormHandler = async (event) => {
 	if (response.ok) {
 		document.location.replace('/geography');
 	} else {
-		alert('Failed to delete project');
+		alert('Failed to update post');
 	}
 	}
 };
 
 
 const delGeoButtonHandler = async (event) => {
-	if (event.target.hasAttribute('geo-data-id')) {
-	const geo_id = event.target.getAttribute('geo-data-id');
+	event.preventDefault();
+	const geo_id = window.location.toString().split('/')[window.location.toString().split('/').length - 1];
 
 	const response = await fetch(`/api/geopost/${geo_id}`, {
 		method: 'DELETE',
@@ -38,8 +37,7 @@ const delGeoButtonHandler = async (event) => {
 	if (response.ok) {
 		document.location.replace('/geography');
 	} else {
-		alert('Failed to delete project');
-	}
+		alert('Failed to delete post');
 	}
 };
 
